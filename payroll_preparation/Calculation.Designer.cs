e491ec9658ef1070bec7b971ce00d8846353c333
EdvKlaus <131393806+EdvKlaus@users.noreply.github.com>
2023-11-30 22:34:14 +0300

    Update Calculation.Designer.cs

--- payroll_preparation/Calculation.Designer.cs
+++ payroll_preparation/Calculation.Designer.cs
@@ -80,11 +80,11 @@
             // label2
             // 
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(88, 148);
+            this.label2.Location = new System.Drawing.Point(88, 144);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(116, 66);
+            this.label2.Size = new System.Drawing.Size(127, 82);
             this.label2.TabIndex = 2;
-            this.label2.Text = "Количество рабочих дней";
+            this.label2.Text = "Количество рабочих дней (в неделю)";
             // 
             // label3
             // 
